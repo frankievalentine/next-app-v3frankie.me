6ef0136686000f3cdb4c3c36bb447bbb5d5341a7
Frankie <26727138+frankievalentine@users.noreply.github.com>
2023-06-18 23:42:33 -0700

    Small updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,31 @@ import "@/styles/globals.css";
 // import Sidebar from "../components/sidebar";
 // import { Analytics } from "@vercel/analytics/react";
 
+// favicon HEAD
+{
+  /* <link rel="apple-touch-icon-precomposed" sizes="57x57" href="https://www.v3frankie.me/apple-touch-icon-57x57.png" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="https://www.v3frankie.me/apple-touch-icon-114x114.png" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="https://www.v3frankie.me/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="https://www.v3frankie.me/apple-touch-icon-144x144.png" />
+<link rel="apple-touch-icon-precomposed" sizes="60x60" href="https://www.v3frankie.me/apple-touch-icon-60x60.png" />
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="https://www.v3frankie.me/apple-touch-icon-120x120.png" />
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="https://www.v3frankie.me/apple-touch-icon-76x76.png" />
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="https://www.v3frankie.me/apple-touch-icon-152x152.png" />
+<link rel="icon" type="image/png" href="https://www.v3frankie.me/favicon-196x196.png" sizes="196x196" />
+<link rel="icon" type="image/png" href="https://www.v3frankie.me/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/png" href="https://www.v3frankie.me/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="https://www.v3frankie.me/favicon-16x16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="https://www.v3frankie.me/favicon-128.png" sizes="128x128" />
+<meta name="application-name" content="v3frankie.me - Frankie Valentine"/>
+<meta name="msapplication-TileColor" content="#FFFFFF" />
+<meta name="msapplication-TileImage" content="https://www.v3frankie.me/mstile-144x144.png" />
+<meta name="msapplication-square70x70logo" content="https://www.v3frankie.me/mstile-70x70.png" />
+<meta name="msapplication-square150x150logo" content="https://www.v3frankie.me/mstile-150x150.png" />
+<meta name="msapplication-wide310x150logo" content="https://www.v3frankie.me/mstile-310x150.png" />
+<meta name="msapplication-square310x310logo" content="https://www.v3frankie.me/mstile-310x310.png" />
+<meta name="msapplication-notification" content="frequency=30;polling-uri=https://notifications.buildmypinnedsite.com/?feed=https://www.v3frankie.me/sitemap.xml&amp;id=1;polling-uri2=https://notifications.buildmypinnedsite.com/?feed=https://www.v3frankie.me/sitemap.xml&amp;id=2;polling-uri3=https://notifications.buildmypinnedsite.com/?feed=https://www.v3frankie.me/sitemap.xml&amp;id=3;polling-uri4=https://notifications.buildmypinnedsite.com/?feed=https://www.v3frankie.me/sitemap.xml&amp;id=4;polling-uri5=https://notifications.buildmypinnedsite.com/?feed=https://www.v3frankie.me/sitemap.xml&amp;id=5;cycle=1" /> */
+}
+
 // const kaisei = localFont({
 //   src: "../public/fonts/kaisei-tokumin-latin-700-normal.woff2",
 //   weight: "700",
